bdc1ed620f71e4972669a66d6677495148303aad
Filip Budiša <filip@circuitmess.com>
2023-09-12 11:24:46 +0200

    Update pins

--- main/src/Pins.hpp
+++ main/src/Pins.hpp
@@ -1,41 +1,41 @@
 #ifndef PERSE_BASICCTRL_PINS_H
 #define PERSE_BASICCTRL_PINS_H
 
-#define PIN_BATT 8
-#define PIN_BL 6
-#define PIN_QUAL 7
+#define PIN_BATT 9
+#define PIN_BL 39
+#define PIN_QUAL 8
 
 #define JOY_H 2
 #define JOY_V 1
 
-#define BTN_PAIR 12
-#define BTN_PANIC 48
-#define BTN_JOY 11
-#define BTN_ENC_CAM 42
-#define BTN_ENC_ARM 36
-#define BTN_ENC_PINCH 39
+#define BTN_PAIR 17
+#define BTN_PANIC 14
+#define BTN_JOY 13
+#define BTN_ENC_CAM 3
+#define BTN_ENC_ARM 11
+#define BTN_ENC_PINCH 6
 
-#define SW_ARM 9
-#define SW_LIGHT 10
+#define SW_ARM 4
+#define SW_LIGHT 44
 
-#define LED_PAIR 15
-#define LED_PANIC1 16
-#define LED_PANIC2 17
+#define LED_PAIR 18
+#define LED_PANIC1 21
+#define LED_PANIC2 26
 
-#define ENC_CAM_A 43
-#define ENC_CAM_B 44
-#define ENC_ARM_A 37
-#define ENC_ARM_B 38
-#define ENC_PINCH_A 40
-#define ENC_PINCH_B 41
+#define ENC_CAM_A 34
+#define ENC_CAM_B 47
+#define ENC_ARM_A 10
+#define ENC_ARM_B 12
+#define ENC_PINCH_A 5
+#define ENC_PINCH_B 7
 
-#define TFT_SCK 0
-#define TFT_MOSI 3
-#define TFT_DC 4
-#define TFT_RST 5
+#define TFT_SCK 40
+#define TFT_MOSI 41
+#define TFT_DC 42
+#define TFT_RST 43
 
-#define I2C_SDA 14
-#define I2C_SCL 13
+#define I2C_SDA 15
+#define I2C_SCL 16
 
 #define EXTLED_CAM_0 0
 #define EXTLED_CAM_L1 1
