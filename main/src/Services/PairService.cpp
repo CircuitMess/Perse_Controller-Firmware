7105de4f8e522b90c5926d77b99f4c20d410bcea
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-19 11:18:42 +0200

    Added PairService

---
+++ main/src/Services/PairService.cpp
@@ -0,0 +1,44 @@
+#include "PairService.h"
+#include "Util/Services.h"
+
+PairService::PairService() : wifi(*(WiFiSTA*) Services.get(Service{/*TODO - set actual service*/})),
+							 tcp(*(TCPClient*) Services.get(Service{/*TODO - set actual service*/})),
+							 thread([this](){ loop(); }, "PairService", 4 * 1024), queue(10){
+
+	wifi.connect();
+	Events::listen(Facility::WiFiSTA, &queue);
+
+}
+
+PairService::~PairService(){
+	Events::unlisten(&queue);
+	if(thread.running()){
+		thread.stop();
+	}
+}
+
+PairService::State PairService::getState() const{
+	return state;
+}
+
+void PairService::loop(){
+	Event event{};
+	if(queue.get(event, portMAX_DELAY)){
+		if(event.facility == Facility::WiFiSTA){
+			auto data = (WiFiSTA::Event*) event.data;
+			if(data->action == WiFiSTA::Event::Connect && data->connect.success){
+				bool res = tcp.connect();
+				if(res){
+					state = State::Success;
+				}else{
+					state = State::Fail;
+				}
+				thread.stop();
+			}else if(data->action == WiFiSTA::Event::Connect && !data->connect.success){
+				state = State::Fail;
+				thread.stop();
+			}
+		}
+		free(event.data);
+	}
+}
