a652846ca13df2a7d9aff4a3fc631d95ca818705
Filip Budiša <filip@circuitmess.com>
2023-10-25 14:31:36 +0200

    Init cleanup

--- main/main.cpp
+++ main/main.cpp
@@ -1,30 +1,34 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <nvs_flash.h>
-#include <driver/gpio.h>
 #include <esp_sleep.h>
-#include <esp_log.h>
-#include "Pins.hpp"
-#include "Util/stdafx.h"
-#include "Util/Services.h"
-#include "Util/Events.h"
 #include "Periph/WiFiSTA.h"
 #include "Periph/SPIFFS.h"
+#include "Devices/Battery.h"
 #include "Devices/Display.h"
 #include "Devices/Backlight.h"
 #include "Services/TCPClient.h"
-#include "Services/RoverState.h"
-#include "Devices/Battery.h"
 #include "Services/Comm.h"
-#include "UISystem/IntroScreen.h"
+#include "Services/RoverState.h"
 #include "UISystem/UIThread.h"
+#include "UISystem/IntroScreen.h"
+#include "Util/Services.h"
+
+[[noreturn]] void shutdown(){
+	ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_AUTO));
+	ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_RC_FAST, ESP_PD_OPTION_AUTO));
+	ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_CPU, ESP_PD_OPTION_AUTO));
+	ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_AUTO));
+	ESP_ERROR_CHECK(esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL));
+	esp_deep_sleep_start();
+}
 
 void init(){
-	gpio_config_t cfg = {
-			.pin_bit_mask = 0,
-			.mode = GPIO_MODE_INPUT
-	};
-	gpio_config(&cfg);
+	auto battery = new Battery();
+	if(battery->isShutdown()){
+		shutdown();
+		return;
+	}
 
 	auto ret = nvs_flash_init();
 	if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND){
@@ -38,15 +42,6 @@ void init(){
 	auto settings = new Settings();
 	Services.set(Service::Settings, settings);
 
-	auto display = new Display();
-	display->drawTest();
-
-	auto bl = new Backlight(LEDC_CHANNEL_0);
-	Services.set(Service::Backlight, bl);
-
-	bl->fadeIn();
-
-	printf("Init done.\n");
 	auto wifi = new WiFiSTA();
 	Services.set(Service::WiFi, wifi);
 	auto tcp = new TCPClient();
@@ -58,23 +53,21 @@ void init(){
 	auto comm = new Comm();
 	Services.set(Service::Comm, comm);
 
-	auto battery = new Battery();
-	battery->begin();
+	auto display = new Display();
+	display->drawTest();
 
-	if (battery->isShutdown()) {
-		ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_AUTO));
-		ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_RC_FAST, ESP_PD_OPTION_AUTO));
-		ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_CPU, ESP_PD_OPTION_AUTO));
-		ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_AUTO));
-		ESP_ERROR_CHECK(esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL));
-		esp_deep_sleep_start();
-		return;
-	}
+	auto bl = new Backlight(LEDC_CHANNEL_0);
+	Services.set(Service::Backlight, bl);
 
 	auto uiThread = new UIThread(*display);
-	uiThread->startScreen(&IntroScreen::createScreen);
-	uiThread->start();
 	Services.set(Service::UI, uiThread);
+
+	uiThread->startScreen(&IntroScreen::createScreen);
+
+	uiThread->start();
+	bl->fadeIn();
+
+	battery->begin();
 }
 
 extern "C" void app_main(void){
