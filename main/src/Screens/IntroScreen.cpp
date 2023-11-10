65f17b55192fffe061e5060868fd4ae8de14149e
Emil Gajšak <emil.gajsak@gmail.com>
2023-11-10 11:03:19 +0100

    IntroScreen animation adjusted

--- main/src/Screens/IntroScreen.cpp
+++ main/src/Screens/IntroScreen.cpp
@@ -6,10 +6,10 @@
 #include "Screens/PairScreen.h"
 
 const std::vector<std::tuple<std::string, uint16_t, uint16_t>> IntroScreen::imageInfos = {
-		{"/spiffs/logo-cm.raw", 91, 91},
-		{"/spiffs/logo-geek.raw", 97, 81},
-		{"/spiffs/logo-space.raw", 116, 29},
-		{"/spiffs/logo-artemis.raw", 105, 14}
+		{ "/spiffs/logo-cm.raw",    91,  91 },
+		{ "/spiffs/logo-geek.raw",  97,  81 },
+		{ "/spiffs/logo-space.raw", 116, 29 },
+		{ "/spiffs/logo-perse.raw", 72,  71 }
 };
 
 const std::string IntroScreen::crossPath = "/spiffs/cross.raw";
@@ -25,13 +25,13 @@ IntroScreen::IntroScreen(Sprite& canvas) : Screen(canvas), lastLoopTime(micros()
 			createMovingImage(std::get<0>(tuple), std::get<1>(tuple), std::get<2>(tuple));
 	}
 
-	setBgColor(C_RGB(backgroundColor.x, backgroundColor.y, backgroundColor.z));
+	setBgColor(backgroundColor);
 }
 
 IntroScreen::~IntroScreen() = default;
 
 void IntroScreen::preDraw() {
-	setBgColor(C_RGB(backgroundColor.x, backgroundColor.y, backgroundColor.z));
+	setBgColor(backgroundColor);
 }
 
 void IntroScreen::onLoop() {
