e64e26435cce2d4a4613d162e39cbf9fc351cd35
Emil Gajšak <emil.gajsak@gmail.com>
2023-12-28 11:49:35 +0100

    Basic controller pers 59 (#36)
    * CMake controller type selection using CTRL_TYPE variable (#24)
    
    * CMake controller type selection using CTRL_TYPE variable
    
    * Moved LV_TICK define to MissionCtrl if-case in CMakeLists.txt
    
    * Code separation pers 61 (#25)
    
    * Separated CMake sources, SPIFFS folders for different controller types
    
    * Separated HW-related definitions by controller type
    
    * CMake setting cache to empty string when setting new variables, LEDService safety check when accessing map
    
    * Changed CMake to use CTRL_TYPE variable from cache, not from environment
    
    * Updated sdkconfig for basic controller
    
    * Fixed CMake includes not working for Basic controller
    
    * Added SinglePwmLED invert param in constructor, LEDService pinMap fixed (#27)
    
    * CMake cleanup, refactoring PERS-74
    
    * Basic controller state machine (#30)
    
    * Added StateMachine, PairState, updated CMake includes
    
    * Fixed LEDBlinkFunction logic
    
    * Added PairService as an Event facility
    
    * Changed PairState to be blocked by Pair events
    
    * Added DriveState, extra parameter in PairState constructor
    
    * Updated Basic controller sources in CMakeLists, minor fixes
    
    * Cleanup, zero drive state sending optimization, added nullptr checks
    
    ---------
    
    Co-authored-by: Fr0zen3k <jan.kresic@circuitmess.com>
    
    * Added BatteryLowService for Basic controller (#31)
    
    * Disabling feed and scanning on basic controller PERS-78
    
    * Added BatteryLowService construction in main.cpp for Basic ctrl
    
    * Master merge, spiffs file conflicts
    
    ---------
    
    Co-authored-by: Fr0zen3k <jan.kresic@circuitmess.com>

---
+++ main/src/States/PairState.cpp
@@ -0,0 +1,65 @@
+#include "PairState.h"
+#include "Util/Services.h"
+#include "DriveState.h"
+#include "Services/LEDService.h"
+
+PairState::PairState(bool connectionError) : State(), evts(10){
+	Events::listen(Facility::Input, &evts);
+	Events::listen(Facility::Pair, &evts);
+
+	if(!connectionError) return;
+	if(auto* led = (LEDService*) Services.get(Service::LED)){
+		led->off(LED::Pair);
+	}
+}
+
+PairState::~PairState(){
+	Events::unlisten(&evts);
+}
+
+void PairState::loop(){
+	Event evt{};
+	if(evts.get(evt, portMAX_DELAY)){
+		if(evt.facility == Facility::Input && evt.data != nullptr){
+			auto data = (Input::Data*) evt.data;
+			processInput(*data);
+		}else if(evt.facility == Facility::Pair && pair){
+			auto success = ((PairService::Event*) evt.data)->success;
+			pair.reset();
+
+			if(success){
+				if(auto* stateMachine = (StateMachine*) Services.get(Service::StateMachine)){
+					stateMachine->transition<DriveState>();
+				}
+			}else{
+				if(auto* led = (LEDService*) Services.get(Service::LED)){
+					led->blink(LED::Pair, 4, 500);
+					led->blink(LED::Warning, 4, 500);
+				}
+			}
+
+			return;
+		}
+		free(evt.data);
+	}
+}
+
+void PairState::processInput(const Input::Data& evt){
+	if(evt.btn != Input::Pair) return;
+
+	auto* led = (LEDService*) Services.get(Service::LED);
+
+	if(evt.action == Input::Data::Press && !pair){
+		pair = std::make_unique<PairService>();
+
+		if(led != nullptr){
+			led->blink(LED::Pair, -1, PairBlinkInterval);
+		}
+	}else if(evt.action == Input::Data::Release && pair){
+		pair.reset();
+
+		if(led != nullptr){
+			led->off(LED::Pair);
+		}
+	}
+}
