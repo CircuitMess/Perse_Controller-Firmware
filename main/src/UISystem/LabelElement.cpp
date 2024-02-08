bf06538e1208db2ba0df77c75463a46d4b8eafb6
Emil Gajšak <emil.gajsak@gmail.com>
2024-02-08 15:34:08 +0100

    Added LabelElement shading to TextStyle

--- main/src/UISystem/LabelElement.cpp
+++ main/src/UISystem/LabelElement.cpp
@@ -13,9 +13,23 @@ void LabelElement::draw(Sprite* canvas){
 
 	canvas->setFont(style.font);
 	canvas->setTextDatum(style.datum);
-	canvas->setTextColor(style.color);
 	canvas->setTextSize(style.scale);
 
+	if(style.shadingStyle.width > 0){
+		canvas->setTextColor(style.shadingStyle.color);
+		for(int i = 1; i <= style.shadingStyle.width; ++i){
+			canvas->drawString(text.c_str(), x + i, y);
+			canvas->drawString(text.c_str(), x - i, y);
+			canvas->drawString(text.c_str(), x, y + i);
+			canvas->drawString(text.c_str(), x, y - i);
+			canvas->drawString(text.c_str(), x + i, y + i);
+			canvas->drawString(text.c_str(), x - i, y + i);
+			canvas->drawString(text.c_str(), x + i, y - i);
+			canvas->drawString(text.c_str(), x - i, y - i);
+		}
+	}
+
+	canvas->setTextColor(style.color);
 	canvas->drawString(text.c_str(), x, y);
 }
 
